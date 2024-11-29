0d913f91dcd8c8bc7dfa90554294d0396632217d
ankitji0811 <ankitjijindal030@gamil.com>
2024-11-29 14:42:55 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const sectionData = [
+  {
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/2ad3/d5c1/bc7c8b7d2ade47a27798d58559b94505?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Py7-0zo-2ULgybdmw5eLb2E6hSrSqtpVkRn3zFRwkkPs8NhNCw76fEwTrhLzl50AKhxdKKVGisZRaG~wJJ-LcpKrl2SKImEWxyP8pUU5FBCtXCk5EKKafB8cpOds8tks2f9APh6IjTkjxPOiyjTT~PkDKCFyCI9hqRPuZnWl1T6Amfav9o0zofIMjiCNp6tstxMegHCTuMMjVtZkzT9B04GH9Ux6tt-BXrR5a5oZlTVrBIjcF9rGLDpfJPGMnqAlmS3g3girl2tYsD-kAWg-KHuC23tOkLgPwzNvG3HKneefpOyaH9FMe-XW8m4SMRSkJQv3Zp98cZcKyOa5~CXjfw__",
+    heading: "Exquisite Watches",
+    subHeading: "Gold Luxury, <span>Choose Us</span>",
+    description:
+      "Discover the Perfect Watch for Every Occasion and Elevate Your Style with Timeless Elegance and Precision Craftsmanship - watch",
+    price: "$499.00",
+  },
+  {
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/9b03/5859/b3cd59b6e82967a441a89bdb7f113601?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=G4IsGh9QklY8Lb8NKdCWMi2zvGCz8753OJzU6WxAb8uOWqPv5eivhL1FgFNupav~sEaof6S4laUBAd~DtD~nGc4FnWhYNSicPQPJW3SrSYOeEloJomZnSDjyZUgFI1rxeoRyZh8ATgFUn3y6BX0PZKWZ4fa0IV3cJkNz4pfuwCIjW2euSOWGnSQOPzZ1CsDmNKExB~LiXJfdnBrJHraTGHP9n3B5sGIe9cxjO2m-TktigbOVFuRxN~ZoYRGvtLlSbmbTwVpTpLUABSZtkayviOb2S-5oGeU2Zb04WM55Oj3JenP50OslrzOvkrmYRj1Qd~JSz9otUf2vQH1Agab-lw__",
+    heading: "Dainty Timepieces",
+    subHeading: "Silver Luxury, <span>Choose Us</span>",
+    description:
+      "Explore the Ideal Timepiece for Any Moment and Enhance Your Style with Timeless Sophistication and Impeccable Craftsmanship - timepiece",
+    price: "$469.00",
+  },
+  {
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/1155/92de/a1e0499b541d8505e2a8b346af7f884f?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gtDtMC~A3zfL37A1ESzLRLv3iMtmHfGmCYf~lIFUUI~djPFI3uxo39UTnp9WvpoQYcqZh0JF4EGmcapT68YAlceulqWubBB9lsyBdS1X0UmCdNhx6v0kXaIMGicZ8gflQPmtdsqpp4XV7WwpG~IkO4Cdc9KCvAW-uEjwKDELbnpwyx3veZ-rbbp~RU8LFWpGwTxAt4NM4MB5ZerL2ya-9we83pUkhmKtwQQRoZ20YJavZWzzW~4-ghjm26dgwUuOkkLxm~BT0~djjrR1D3aa2AMbXP8de~n5PX6XtVgp2WJS4EpgUlTUvze6nlUuWjvNqOZ2D7y5xBfBMQNfz6UENw__",
+    heading: "Exquisite Watches",
+    subHeading: "Gold Luxury, <span>Choose Us</span>",
+    description:
+      "Discover the Perfect Watch for Every Occasion and Elevate Your Style with Timeless Elegance and Precision Craftsmanship - watch",
+    price: "$529.00",
+  },
+  {
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/c62f/050a/5112960cf7b3fd65ba1ed29d826e710a?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DZ6wKk0KGbYps-0kGuXg0hpOzr9sECM0k-QqzyQrYgVgR5b0Kp~ZHLx0oGtTVH-aFLPm93HKloN6j9Ke~5-mKYqdYJc3wSlymedqrk3t56Jzw8Jf-pRI-3BheEdl1RjCY0SZ5hAso~ysII9zUNgxTKe8KNz1pVCTwJn57XM-Ba34vHxY0rtd0lMnBdQLFa0n4hFzeT~CRCvjJaguI-0KlMO2zA0oo2rO0PCnbfeQBt8bXtfOm~-08mAPyZAyoUucTpnm8UxbihmZL1WjOrJNlzOII-7RkRd6jJ3IkwwzHkymQefmpOPSPRgDYWO-h44uszVzD5K6uB0sL9jRK1ed1A__",
+    heading: "Refined Timepieces",
+    subHeading: "Choose Luxury, <span>Choose Us</span>",
+    description:
+      "Explore the Ideal Timepiece for Any Moment and Enhance Your Style with Timeless Sophistication and Impeccable Craftsmanship - timepiece",
+    price: "$599.00",
+  },
+];
+
+const bgColors = [
+  "linear-gradient(to right, #F4AB6B 0%, #FFDEC2 100%)",
+  "linear-gradient(to right, #B3B6B6 0%, #E1E1E1 100%)",
+  "linear-gradient(to right, #34A75B 0%, #74E299 100%)",
+  "linear-gradient(to right, #F25856 0%, #FEA794 100%)",
+];
+
+const splideSection = document.querySelector(".splide");
+
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide", {
+    type: "fade",
+    pagination: false,
+  });
+  splide.mount().on("move", (index) => {
+    document.body.style.background = bgColors[index];
+  });
+});
+
+const slider_list = document.querySelector(".splide__list");
+
+sectionData.forEach((data) => {
+  const splide = document.createElement("li");
+  splide.classList.add("splide__slide");
+
+  splide.innerHTML = `<div class="main-content">
+    <h1>${data.heading}</h1>
+    <h2>${data.subHeading}</h2>
+    <p>${data.description}</p>
+    <div>${data.price}</div>
+    </div>
+    <div class="main-img">
+    <img src="${data.imageUrl}"></div>`;
+
+  splide.style.backgroundColor = `${data.backGround}`;
+
+  slider_list.appendChild(splide);
+});
